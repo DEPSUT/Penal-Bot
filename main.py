9bb2a8c9e7cf00d4d32f8757fb599111d5807bd1
DEPSUT <77130196+DEPSUT@users.noreply.github.com>
2021-12-09 00:51:39 +0000

    Some bug fixes
    Fixed the yoda bug where it wouldn't send the message
    Fixed the whatis prefix command where it would change the prefix

--- main.py
+++ main.py
@@ -12,13 +12,35 @@ client = discord.Client()
 # Variables used
 x = 0
 totalCommands = 0
-text = ''
+global prefix # Global just to be acessed easily anywhere
 
-# Gets the contents of config.json file so the token is not revealed
+# Gets the contents of config.json file
 with open('config.json') as f:
     data = json.load(f)
     token = data['token']
     prefix = data['prefix']
+#--------------------------------------------------------------------------------------------------------------------------#
+
+# Function that changes the prefix
+def change_prefix(prefix):
+    f = open("config.json", "r") # Open the json file for reading
+    data = json.load(f) # Read the json
+    f.close() # Close the json file
+
+    # Atualiza o ficheiro config.json com o novo prefixo
+    data["prefix"] = prefix
+
+    # Save the changes to the json file
+    f = open("config.json", "w+")
+    f.write(json.dumps(data))
+    f.close()
+
+    # Update our changes 
+    with open('config.json') as f:
+        data = json.load(f)
+        prefix = data['prefix']
+    
+    return prefix
 #--------------------------------------------------------------------------------------------------------------------------#
 
 # Function that returns a "yo mamma" joke
@@ -39,10 +61,10 @@ def get_kanye_quote():
 def translate_yoda(text):
     response = requests.get('https://api.funtranslations.com/translate/yoda.json?text='+urllib.parse.quote(text)) # Converts the text into a URL string
     data = json.loads(response.text)
-    if data['error'] is None:
-        return data['contents']['translated']
-    else:
+    if 'error' in data: # Checks if error exists in the json file
         return data['error']['message']
+    else:
+        return data['contents']['translated']
 #--------------------------------------------------------------------------------------------------------------------------#
 
 # Shows when the bot has logged in and what bot has logged in
@@ -57,10 +79,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    message.content = message.content.lower()
+    message.content = message.content.lower() # Makes every message lowercase
 
     global x
     global totalCommands
+    global prefix
 
     # If the bot sends the message the function isn't executed
     if message.author == client.user:
@@ -76,6 +99,22 @@ async def on_message(message):
     if message.content.startswith(prefix+'ola') or message.content.startswith(prefix+'olá'):
         await message.channel.send('Olá!')
         print("Sent message: 'Olá'")
+        x += 1 # Counts +1 if the command is used
+
+    # Changes/Shows the prefix of the bot
+    if message.content.startswith(prefix+'prefix') or message.content.startswith('whatis prefix'):
+        if message.content == prefix+'prefix' or message.content == 'whatis prefix':
+            with open('config.json') as f: # Updates the prefix just incase
+                data = json.load(f)
+                prefix = data['prefix']
+            await message.channel.send("The prefix is '"+prefix+"'")
+            print("Sent message: 'The prefix is  '"+prefix+"''")
+        elif  prefix+'prefix' in message.content:
+            message.content = message.content.replace(prefix+'prefix','') # Removes the call command
+            message.content = message.content.strip() # Removes whitespaces from the string
+            prefix = change_prefix(message.content)
+            await message.channel.send("Changed prefix to '"+message.content+"'")
+            print("Sent message: 'Changed prefix to '"+message.content+"''")
         x += 1 # Counts +1 if the command is used
 
     # Sends a "yo mamma" joke using the get_joke() function
@@ -94,13 +133,13 @@ async def on_message(message):
     # Sends a Kanye West quote using the kanye.rest API
     if message.content.startswith(prefix+'kanye'):
         quote = get_kanye_quote()
-        await message.channel.send(quote)
+        await message.channel.send(quote+' - Kanye West')
         print("Sent message: 'Kanye Quote'")
         x += 1 # Counts +1 if the command is used
 
     # Translates the message sent to yoda
     if message.content.startswith(prefix+'yoda'):
-        message.content = message.content.replace('€yoda','')
+        message.content = message.content.replace(prefix+'yoda ','')
         text = translate_yoda(message.content) # Sends the message to the function
         await message.channel.send(text)
         print("Sent message: 'Yoda Translation'")
